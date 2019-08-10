2b33d821dda18955c18ae6976b9eec210dda80f8
Joe Robertson <jwj.robert@gmail.com>
2019-08-10 16:54:00 -0400

    Update gitignore and configure boilerplate views for handlebars

--- copy-assets.ts
+++ copy-assets.ts
@@ -1,11 +1,6 @@
 import * as shell from 'shelljs';
 
-shell.mkdir('-p', 'src/assets/libs');
-shell.mkdir('-p', 'src/assets/fonts');
-shell.mkdir('-p', 'src/assets/images');
-shell.mkdir('-p', 'src/assets/styles');
-shell.mkdir('-p', 'src/assets/scripts');
-shell.cp('-R', 'src/assets/libs', 'dist/assets/libs');
-shell.cp('-R', 'src/assets/fonts', 'dist/assets/fonts');
-shell.cp('-R', 'src/assets/images', 'dist/assets/images');
-shell.cp('-R', 'src/assets/scripts', 'dist/assets/scripts');
+shell.cp('-R', 'src/assets/libs', 'dist/assets');
+shell.cp('-R', 'src/assets/fonts', 'dist/assets');
+shell.cp('-R', 'src/assets/images', 'dist/assets');
+shell.cp('-R', 'src/assets/scripts', 'dist/assets');
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import path from 'path';
+import exphbs from 'express-handlebars';
 
 const app = express();
 
+app.set('views', path.join(__dirname, '../src/views'));
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  layoutsDir: path.join(__dirname, '../src/views/layouts'),
+  defaultLayout: 'main.hbs',
+  partialsDir: 'partials'
+}));
 app.set('view engine', 'hbs');
+
 app.get('/', (req, res): void => {
-  res.send('Hello World!');
+  res.render('pages/index');
 });
 
 app.listen(8082, (): void => {
